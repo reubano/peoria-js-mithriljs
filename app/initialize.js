689e75142cd4818afe5c5598b54caa58a9784096
Reuben Cummings <reubano@gmail.com>
2018-10-01 14:52:50 -0500

    Replace react w mithril

--- app/initialize.js
+++ app/initialize.js
@@ -1,7 +1,6 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import App from "todo";
+var m = require("mithril");
+var App = require("todo");
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(<App />, document.getElementById("app"));
+  m.mount(document.getElementById("app"), App);
 });
